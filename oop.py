2a7da01c9f77970a56a303e1391588b65e0198c5
Riddhub <unkownrid@gmail.com>
2022-07-01 15:04:08 +0300

    add new lessions

--- oop.py
+++ oop.py
@@ -80,7 +80,165 @@
 # f1.reduce()
 
 
+# class Point:
+#     "New class"
+#     color = 'red'
+#     circle = 2
+#
+#  a.__dict__
+# Point.__doc__
+#
+# Point.type_pt = 'disc'
+# setattr(Point, 'type_pt', 'square')
+#
+# Point.color
+# getattr(Point, 'a', False)
+#
+# hasattr(Point, 'prob')
+#
+# del Point.prop
+# delattr(Point, 'type_pt')
 
 
+# class Point:
+#     "New class"
+#     color = 'red'
+#     circle = 2
+#
+#     def set_coords(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     def get_coords(self):
+#         return (self.x, self.y)
+#
+#
+# pt = Point()
+# pt2 = Point()
+# pt.set_coords(1, 2)
+# pt2.set_coords(10, 20)
+# print(pt.__dict__)
+# print(pt2.__dict__)
+#
+# print(pt.get_coords())
+# f = getattr(pt, 'get_coords')
+# print(f())
+
+######################## INITIALIZATION AND FINALIZATION ##################
+
+# class Point:
+#     'New class'
+#     color = 'red'
+#     circle = 2
+#
+#     def __init__(self, x=0, y=0):
+#         print('call __init__')
+#         self.x = x
+#         self.y = y
+#
+#     def __del__(self):
+#         print('Deleting instance: ' + str(self))
+#
+#     def set_coords(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     def get_coords(self):
+#         return self.x, self.y
+#
+#
+# pt = Point(1, 2)
+# print(pt.__dict__)
+
+
+###########################  MAGIC METHOD #####################
+
+# class Point:
+#     'New class'
+#     color = 'red'
+#     circle = 2
+#
+#     def __new__(cls, *args, **kwargs):
+#         print('call __new__ for ' + str(cls))
+#         return super().__new__(cls)
+#
+#     def __init__(self, x=0, y=0):
+#         print('call __init__ for ')
+#         self.x = x
+#         self.y = y
+#
+#
+# pt = Point(1, 2)
+# print(pt)
+
+
+#######################  SINGLETON (SIMPLE) ###############################
+
+# class Database:
+#     __instance = None
+#
+#     def __new__(cls, *args, **kwargs):
+#         if cls.__instance is None:
+#             cls.__instance = super().__new__(cls)
+#
+#         return cls.__instance
+#
+#     def __del__(self):
+#         Database.__instance = None
+#
+#     def __init__(self, user, psw, port):
+#         self.user = user
+#         self.psw = psw
+#         self.port = port
+#
+#     def connect(self):
+#         print(f'connecting witt DB: {self.user}, {self.psw}, {self.port}')
+#
+#     def close(self):
+#         print('close connect with DB')
+#
+#     def read(self):
+#         return 'data from DB'
+#
+#     def write(self, data):
+#         print(f'write in DB {data}')
+#
+# db = Database('root', '1234', 80)
+# db2 = Database('root2', '124', 40)
+# print(id(db), id(db2))
+# print(db.__dict__)
+
+
+######################### DECARATION:  Methods ######################
+
+# class Vector:
+#     MIN_COORD = 0
+#     MAX_COORD = 100
+#
+#     @classmethod
+#     def validate(cls, arg):
+#         return cls.MIN_COORD <= arg <= cls.MAX_COORD
+#
+#     def __init__(self, x, y):
+#         self.x = self.y = 0
+#         if self.validate(x) and self.validate(y):
+#             self.x = x
+#             self.y = y
+#
+#         print(self.norm2(self.x, self.y))
+#
+#     @staticmethod
+#     def norm2(x, y):
+#         return x*x + y*y
+#
+#     def get_coord(self):
+#         return self.x, self.y
+#
+#
+# v = Vector(1, 20)
+# gc = Vector.get_coord(v)
+# print(gc)
+# print(Vector.validate(5))
+# print(Vector.norm2(5, 6))
 
 
